5f948a0dffc537c7fbaa8f41a77f01b0c7182869
Tanya <t.rozanova@icloud.com>
2025-11-06 10:49:37 +0300

    Добавили вывод переменных

--- lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Main.kt
+++ lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Main.kt
@@ -2,4 +2,15 @@ package ru.tbank.education.school.lesson1
 
 fun main() {
     println("Hello World")
+
+    val a = 123
+    val b: Short? = null
+    var c = 12
+    c++
+    println("Hello $a")
+    println("Hello $c")
+    println("Hello $b")
+
+
+
 }
